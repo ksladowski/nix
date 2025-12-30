58d6a9f50f92a671aea87cfc0604c883f8aa2be5
Kevin Sladowski <ksladowski@mailbox.org>
2025-12-30 14:32:01 -0600

    finished discord config

--- home/common/discord.nix
+++ home/common/discord.nix
@@ -7,6 +7,25 @@
   programs.nixcord = {
     enable = true;
     discord.enable = false; # don't want normal discord client
-    vesktop.enable = true;
+    vesktop ={
+      enable = true;
+      settings.arRPC = false;
+      state.firstLaunch = false;
+    };
+    config.plugins = {
+      anonymiseFileNames.enable = true;
+      ircColors.enable = true;
+      replaceGoogleSearch = {
+        enable = true;
+        customEngineName = "Searx";
+        customEngineURL = "https://searx.sladnet.xyz/search?q=";
+        replacementEngine = "custom";
+      };
+      noTypingAnimation.enable = true;
+      webKeybinds.enable = true;
+      youtubeAdblock.enable = true;
+      webScreenShareFixes.enable = true;
+      volumeBooster.enable = true;
+    };
   };
 }
