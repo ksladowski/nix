d83c0fb7672a732da7791607ae7c3a8ef13e9059
Kevin Sladowski <ksladowski@mailbox.org>
2026-01-02 20:26:29 -0600

    remove root ssh

--- system/common/ssh.nix
+++ system/common/ssh.nix
@@ -4,18 +4,9 @@
 		settings = {
 			PasswordAuthentication = false;
 			KbdInteractiveAuthentication = false;
-			#PermitRootLogin = "no";
-			#AllowUsers = [ "kevin" ];
+			PermitRootLogin = "no";
+			AllowUsers = [ "kevin" ];
 		};
-		# hostKeys = [
-		# {
-		#   path = "/persist/etc/ssh/ssh_host_ed25519_key";
-		#   type = "ed25519";
-		# }
-		# ];
 	};
 
-    users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGuI5zoHAtiH1KjV1QZUh+eZ9r50GrMH7Kjgb45+h0FA KevinSladowski"
-    ];
 }
