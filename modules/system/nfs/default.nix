69ad63916d2e621766fa89534cb5881733897cfa
Kevin Sladowski <ksladowski@mailbox.org>
2026-01-09 21:03:20 -0600

    fixed nfs share hostname

--- modules/system/nfs/default.nix
+++ modules/system/nfs/default.nix
@@ -2,7 +2,7 @@
 {
 	boot.supportedFilesystems = [ "nfs" ];
 	fileSystems."/mnt/media" = {
-		device = "truenas.local:/mnt/tank0/media";
+		device = "truenas.lan:/mnt/tank0/media";
 		fsType = "nfs";
 		options = [ 
 			"x-systemd.automount" 
