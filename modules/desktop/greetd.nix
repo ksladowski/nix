f53bc74011ae7274186d184b1556baf48a43fc78
Kevin Sladowski <ksladowski@mailbox.org>
2026-01-26 17:32:56 -0600

    remove tuigreet, autologin

--- modules/desktop/greetd.nix
+++ modules/desktop/greetd.nix
@@ -1,27 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 {
 
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet \
-         --time --time-format '%I:%M %p | %a - %h | %F' \
-          --cmd 'niri-session'";
-        user = "greeter";
+        command = "niri-session";
+        user = "kevin";
       };
     };
   };
-
-  users.users.greeter = {
-    isNormalUser = false;
-    description = "greetd greeter user";
-    extraGroups = [
-      "video"
-      "audio"
-    ];
-    linger = true;
-  };
-
-  environment.systemPackages = with pkgs; [ tuigreet ];
 }
