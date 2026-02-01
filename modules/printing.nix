3b372735d3b2c2c097c1acc2a6db5db056297ea1
Kevin Sladowski <ksladowski@mailbox.org>
2026-02-01 15:32:00 -0600

    configure printer

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ pkgs, baseVars, ... }:
 {
-  services.printing.enable = true;
-
-  services.avahi = {
+  services.printing = {
     enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
+    drivers = [
+      pkgs.cnijfilter2
+    ];
   };
+
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Canon_MX492";
+        deviceUri = "usb://Canon/MX490%20series?serial=68E8EC&interface=1";
+        model = "canonmx490.ppd";
+      }
+    ];
+    ensureDefaultPrinter = "Canon_MX492";
+  };
+
+  users.groups.lpadmin.members = [ baseVars.username ];
+
 }
--- modules/scanning.nix
+++ modules/scanning.nix
@@ -1,7 +1,11 @@
-{ baseVars, ... }:
+{ pkgs, baseVars, ... }:
 {
   hardware.sane.enable = true;
 
   users.groups.scanner.members = [ baseVars.username ];
   users.groups.lp.members = [ baseVars.username ];
+
+  hm.home.packages = with pkgs; [
+    simple-scan
+  ];
 }
